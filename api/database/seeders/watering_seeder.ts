8715bf64458ee7b789e47e7a20877a902e2752e0
Jeanne Desmier <jeanne@kinoba.fr>
2024-06-10 16:38:12 +0200

    refactor: seed

--- api/database/seeders/watering_seeder.ts
+++ api/database/seeders/watering_seeder.ts
@@ -6,10 +6,21 @@ export default class extends BaseSeeder {
   async run() {
     await Watering.createMany([
       { roomId: 1, wateringDate: DateTime.local().minus({ days: 1 }), watering_user: 'John Doe' },
-      { roomId: 2, wateringDate: DateTime.local().minus({ days: 2 }), watering_user: 'Jane Smith' },
+      { roomId: 2, wateringDate: DateTime.local().minus({ days: 6 }), watering_user: 'Jane Smith' },
       {
         roomId: 3,
         wateringDate: DateTime.local().minus({ days: 3 }),
+        watering_user: 'Alice Brown',
+      },
+      { roomId: 1, wateringDate: DateTime.local().minus({ days: 1 }), watering_user: 'Jane Smith' },
+      {
+        roomId: 2,
+        wateringDate: DateTime.local().minus({ days: 4 }),
+        watering_user: 'Alice Brown',
+      },
+      {
+        roomId: 3,
+        wateringDate: DateTime.local().minus({ days: 9 }),
         watering_user: 'Alice Brown',
       },
     ])
